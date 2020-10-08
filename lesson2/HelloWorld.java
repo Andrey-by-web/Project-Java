6d47843374b691447f2612340217f90ddcbd3fdd
Andrey-by-web <andreybibikoff@gmail.com>
2020-10-09 01:15:07 +0300

    variebles

---
+++ lesson2/HelloWorld.java
@@ -0,0 +1,16 @@
+package lesson2;
+
+import java.util.Scanner;
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("введите число: ");
+        int numberOne = sc.nextInt();
+        System.out.println("введите второе число: ");
+        int numberTwo = sc.nextInt();
+        int sumResult = numberOne + numberTwo;
+        System.out.println(sumResult);
+    }
+}
